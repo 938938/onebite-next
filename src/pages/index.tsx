2f08c8a75f99f4b93d800365b233d67235befc37
wren938 <wren.kwak@peopet.co.kr>
2024-09-17 22:52:55 +0900

    [FEAT] 2.11 SSR 소개 및 실습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 // CSS Module
-import SearchableLayout from "@/components/searchable-layout";
-import style from "./index.module.css";
-import { ReactNode } from "react";
-import books from "@/mock/books.json";
-import BookItem from "@/components/book-item";
+import SearchableLayout from '@/components/searchable-layout';
+import style from './index.module.css';
+import { ReactNode } from 'react';
+import books from '@/mock/books.json';
+import BookItem from '@/components/book-item';
+import { InferGetServerSidePropsType } from 'next';
 
-export default function Home() {
+export const getServerSideProps = () => {
+  // ssr로 동작하도록 자동으로 설정(getServerSideProps)
+  const data = 'hello';
+  return {
+    props: {
+      data,
+    },
+  };
+  // 반드시 객체타입의 props가 들어있어야만 함.
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
   return (
     <div className={style.container}>
       <section>
